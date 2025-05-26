a56db7bb14ba0a46683770efa6329aef4958276e
Ali Ibrahim <alibra71@gmail.com>
2025-05-26 12:14:21 +0300

    cleanup

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -3,8 +3,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
+
 const BREVO_API_KEY = process.env.BREVO_API_KEY;
 const LIST_ID = 3;
 
